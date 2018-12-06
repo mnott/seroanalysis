be5e3ad0366d5c72e84b39a808f2d0aa5d761754
Matthias Nott <matthias.nott@sap.com>
2018-12-07 00:08:00 +0100

    Simplify
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

--- python-process.py
+++ python-process.py
@@ -10,8 +10,12 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-from pandasql import sqldf, load_meat, load_births
-pysqldf = lambda q: sqldf(q, globals())
+
+#
+# Import our own helpers
+#
+from helpers.helpers import loadExcel, queryDataFrame, parseDateTime, asPDF
+
 
 ########################################################################
 ##
@@ -20,46 +24,41 @@ pysqldf = lambda q: sqldf(q, globals())
 ########################################################################
 
 
+########################################################################
+#
+# Load waste data frame
+#
+ff_waste = loadExcel('data/Wastes_August_september_m.xlsx', '9000_E06', ['Date', 'Quantity'])
+
 #
 # Calculate Waste as sum by Day
 #
-wbook = pd.ExcelFile('data/Wastes_August_september_m.xlsx')
-sheet = wbook.parse('9000_E06')
-cols = ['Date', 'Quantity']
-df = pd.DataFrame(data=sheet, columns = cols)
-qry = "select Date, sum(Quantity)*5 as Waste from df group by Date"
-dfout = pysqldf(qry)
-print(df)
-dfout['Date'] = dfout['Date'].map(lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S.%f"))
-plot = dfout.plot(x='Date', y='Waste')
-plot.get_figure().savefig('data/01-waste-time.pdf', format='pdf')
+df = queryDataFrame(ff_waste, 'select Date, sum(Quantity)*5 as Waste from df group by Date')
+df = parseDateTime(df, 'Date', "%Y-%m-%d %H:%M:%S.%f")
+# print(df)
+
+plot = df.plot(x='Date', y='Waste')
+asPDF(plot, 'data/01-waste-time.pdf')
 
 
+########################################################################
+#
+# Load Defects
+#
+ff_defects = loadExcel('data/inspection_results_and_defects_August_september.xlsx', 'Defects', ['Date', 'NUM_DEFECTOS'])
+df2 = queryDataFrame(ff_defects, 'select Date, sum(NUM_DEFECTOS) as DefectCount from df group by Date')
+df2 = parseDateTime(df2, 'Date', "%Y-%m-%d %H:%M:%S.%f")
+# print(df2)
+
+plot = df2.plot(x='Date', y='DefectCount')
+asPDF(plot, 'data/02-defects-time.pdf')
 
 #
-# Calculate defect count as sum by Day
+# Correlate between Waste and Defect Count
 #
-wbook2 = pd.ExcelFile('data/inspection_results_and_defects_August_september.xlsx')
-sheet2 = wbook2.parse('Defects')
-cols2 = ['Date', 'NUM_DEFECTOS']
-df2 = pd.DataFrame(data=sheet2, columns = cols2)
-qry2 = "select Date, sum(NUM_DEFECTOS) as DefectCount from df2 group by Date"
-dfout2 = pysqldf(qry2)
-print(dfout2)
-dfout2['Date'] = dfout2['Date'].map(lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S.%f"))
-plot = dfout2.plot(x='Date', y='DefectCount')
-plot.get_figure().savefig('data/02-defects-time.pdf', format='pdf')
+df_waste_defects = pd.merge(df, df2, on='Date', how='inner')
+print(df_waste_defects)
 
-
-#
-# Join the two data frames
-#
-df_merged = pd.merge(dfout, dfout2, on='Date', how='inner')
-print(df_merged)
-
-#
-# Correlate betwen quantity and defects: Moderate (47%)
-#
 #
 # Correlation Coefficient          Descriptor
 #
@@ -70,63 +69,45 @@ print(df_merged)
 #   .10 - .29                      Low
 #   .01 - .09                      Trivial
 #
-df_merged['Waste'].corr(df_merged['DefectCount']) # 47%
-plot = df_merged.plot(x='Date')
-plot.get_figure().savefig('data/03-waste-defects.pdf', format='pdf')
+df_waste_defects['Waste'].corr(df_waste_defects['DefectCount']) # 47%
+plot = df_waste_defects.plot(x='Date')
+asPDF(plot, 'data/03-waste-defects.pdf')
 
 
+########################################################################
 #
-# Calculate defect count as sum by Day
+# Load Inspection Results
 #
-wbook3 = pd.ExcelFile('data/inspection_results_and_defects_August_september.xlsx')
-sheet3 = wbook3.parse('Inspection results')
-cols3 = ['Date', 'Val']
-df3 = pd.DataFrame(data=sheet3, columns = cols3)
+ff_results = loadExcel('data/inspection_results_and_defects_August_september.xlsx', 'Inspection results', ['Date_Time', 'Date', 'Val', 'Feature', 'Media'])
 
-df4 = df3.copy()
-df4 = df4[df4['Val'] == 'R']
-df3 = df4.copy()
+df_revisions = queryDataFrame(ff_results, "select Date, count(Val)*100 as Revisions from df where Val = 'R' group by Date")
+df_revisions = parseDateTime(df_revisions, 'Date', "%Y-%m-%d %H:%M:%S.%f")
 
-qry3 = "select Date, count(Val)*100 as Revisions from df3 group by Date"
-dfout3 = pysqldf(qry3)
-print(dfout3)
-dfout3['Date'] = dfout3['Date'].map(lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S.%f"))
-plot = dfout3.plot(x='Date', y='Revisions')
-plot.get_figure().savefig('data/04-revisions-time.pdf', format='pdf')
+plot = df_revisions.plot(x='Date', y='Revisions')
+asPDF(plot, 'data/04-revisions-time.pdf')
 
 
-
+########################################################################
 #
-# Join the two data frames
+# Create merged data frame for Correlations
 #
-df_merged2 = pd.merge(dfout2, dfout3, on='Date', how='inner')
-print(df_merged2)
-
-#
-# Put in the Waste as well
-#
-df_merged3 = pd.merge(df_merged2, dfout, on='Date', how='inner')
-print(df_merged3)
+df_merged = pd.merge(df_waste_defects, df_revisions, on='Date', how='inner')
+# print (df_merged)
 
 ## See R vs Defects => Moderate Correlation (40%)
-df_merged3['DefectCount'].corr(df_merged3['Revisions'])
-plot = df_merged3.plot(x='Date', y=['DefectCount', 'Revisions'])
-plot.get_figure().savefig('data/05-defects-revisions-time.pdf', format='pdf')
+df_merged['DefectCount'].corr(df_merged['Revisions'])
+plot = df_merged.plot(x='Date', y=['DefectCount', 'Revisions'])
+asPDF(plot, 'data/05-defects-revisions-time.pdf')
 
 ## See Waste vs. Defects => Moderate Correlation (44%)
-df_merged3['Waste'].corr(df_merged3['DefectCount'])
-plot = df_merged3.plot(x='Date', y=['Waste', 'DefectCount'])
-plot.get_figure().savefig('data/06-defects-waste-time.pdf', format='pdf')
-
+df_merged['Waste'].corr(df_merged['DefectCount'])
+plot = df_merged.plot(x='Date', y=['Waste', 'DefectCount'])
+asPDF(plot, 'data/06-defects-waste-time.pdf')
 
 ## See Waste vs. R => Trivial Correlation (6%)
-df_merged3['Waste'].corr(df_merged3['Revisions'])
-plot = df_merged3.plot(x='Date', y=['Waste', 'Revisions'])
-plot.get_figure().savefig('data/07-waste-revisions-time.pdf', format='pdf')
-
-
-
-
+df_merged['Waste'].corr(df_merged['Revisions'])
+plot = df_merged.plot(x='Date', y=['Waste', 'Revisions'])
+asPDF(plot, 'data/07-waste-revisions-time.pdf')
 
 
 ########################################################################
@@ -135,51 +116,45 @@ plot.get_figure().savefig('data/07-waste-revisions-time.pdf', format='pdf')
 ##
 ########################################################################
 
-wbook4 = pd.ExcelFile('data/inspection_results_and_defects_August_september.xlsx')
-sheet4 = wbook4.parse('Inspection results')
-cols4 = ['Date', 'Feature', 'Media']
-df4 = pd.DataFrame(data=sheet4, columns = cols4)
-
 # Average over each day
-qry4 = "select Date, avg(Media)*200 as 'Temp' from df4 where Feature == 'TEMPERATURA SALA' and Media < 100 group by Date"
-dfout4 = pysqldf(qry4)
-print(dfout4)
-dfout4['Date'] = dfout4['Date'].map(lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S.%f"))
-plot = dfout4.plot(x='Date', y='Temp')
-plot.get_figure().savefig('data/10-temp-time.pdf', format='pdf')
+df_temp = queryDataFrame(ff_results, "select Date, avg(Media) as 'Temp' from df where Feature == 'TEMPERATURA SALA' and Media < 100 group by Date")
+df_temp = parseDateTime(df_temp, 'Date', "%Y-%m-%d %H:%M:%S.%f")
+
+plot = df_temp.plot(x='Date', y='Temp')
+asPDF(plot, 'data/10-temp-time.pdf')
 
 # Don't Average over each day
-cols5 = ['Date_Time', 'Feature', 'Media']
-df5 = pd.DataFrame(data=sheet4, columns = cols5)
+df_temp_all = queryDataFrame(ff_results, "select Date_Time, Media as 'Temp' from df where Feature == 'TEMPERATURA SALA' and Media < 100")
+plot = df_temp_all.plot(x='Date_Time', y='Temp')
+asPDF(plot, 'data/11-temp-time.pdf')
 
-qry5 = "select Date_Time, Media as 'Temp' from df5 where Feature == 'TEMPERATURA SALA' and Media < 100"
-dfout5 = pysqldf(qry5)
-print(dfout5)
-dfout5['Date_Time'] = dfout5['Date_Time'].map(lambda x: datetime.strptime(str(x), "%d.%m.%y %H:%M:%S"))
-plot = dfout5.plot(x='Date_Time', y='Temp')
-plot.get_figure().savefig('data/11-temp-time.pdf', format='pdf')
+#
+# Scale Average Temperature
+#
+df_temp_scaled = queryDataFrame(ff_results, "select Date, avg(Media) * 200 as 'Temp' from df where Feature == 'TEMPERATURA SALA' and Media < 100 group by Date")
+df_temp_scaled = parseDateTime(df_temp_scaled, 'Date', "%Y-%m-%d %H:%M:%S.%f")
 
 
 #
-# Correlate Defects and Temperature
+# Correlate Temperature vs. Defects, Waste, Revisions
 #
-df_merged4 = pd.merge(dfout4, dfout2, on='Date', how='inner')
-print(df_merged4)
-
-## See Temp vs Defects => low (18%)
-df_merged4['Temp'].corr(df_merged2['DefectCount'])
-plot = df_merged4.plot(x='Date', y=['Temp', 'DefectCount'])
-
-plot.get_figure().savefig('data/12-temp-defects-time.pdf', format='pdf')
+df_merged = pd.merge(df_merged, df_temp_scaled, on='Date', how='inner')
 
 
+## Correlate Temp vs Defects
+df_merged['Temp'].corr(df_merged['DefectCount']) # 34%
+plot = df_merged.plot(x='Date', y=['Temp', 'DefectCount'])
+asPDF(plot, 'data/12-temp-defects-time.pdf')
 
+## Correlate Temp vs Waste
+df_merged['Temp'].corr(df_merged['Waste']) # 19%
+plot = df_merged.plot(x='Date', y=['Temp', 'Waste'])
+asPDF(plot, 'data/13-temp-waste-time.pdf')
 
-
-
-
-
-
+## Correlate Temp vs Revisions
+df_merged['Temp'].corr(df_merged['Revisions']) # 28%
+plot = df_merged.plot(x='Date', y=['Temp', 'Revisions'])
+asPDF(plot, 'data/14-temp-revisions-time.pdf')
 
 
 
